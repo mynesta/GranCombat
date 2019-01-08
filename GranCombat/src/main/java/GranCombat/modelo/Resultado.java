ceb9b7a63563adc4d25dcace172347ab88307fd2
DESKTOP-C491Q1L\LoWs-top <sergio.borrell.flores@gmail.com>
2019-01-08 14:28:34 +0100

    Funcion Random en Resultado

--- GranCombat/src/main/java/GranCombat/modelo/Resultado.java
+++ GranCombat/src/main/java/GranCombat/modelo/Resultado.java
@@ -1,5 +1,22 @@
 package GranCombat.modelo;
 
 public class Resultado {
+	
+	
+	static boolean exitoAtacar (int Probabilidad) {
+		int numero;
+		numero = (int) (Math.random() * 100) + 1;
+		if (numero<=Probabilidad) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+	//public int combat() {
+		
+	//}
 
 }
