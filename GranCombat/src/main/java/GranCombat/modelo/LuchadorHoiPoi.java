a9dfcbd27a4f61172cbd19da470b84747213ed8c
DESKTOP-C491Q1L\LoWs-top <sergio.borrell.flores@gmail.com>
2019-01-08 14:11:56 +0100

    HoiPoi + Namac con herencia

--- GranCombat/src/main/java/GranCombat/modelo/LuchadorHoiPoi.java
+++ GranCombat/src/main/java/GranCombat/modelo/LuchadorHoiPoi.java
@@ -1,5 +1,17 @@
 package GranCombat.modelo;
 
-public class LuchadorHoiPoi {
+public class LuchadorHoiPoi extends Luchador{
 
+	public LuchadorHoiPoi(String nombre, int forz, int cons, int mid, int des, int per) {
+		super(nombre, forz, cons, mid, des, per);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public int ProbabilitatEsquivar(int destreça){
+		return ProbabilitatEsquivarHoiPoi (destreça, super.per);
+	}
+	
+	public int ProbabilitatEsquivarHoiPoi(int destreça ,int personalitat){
+		return (destreça*3)+personalitat;
+	}
 }
--- GranCombat/src/main/java/GranCombat/modelo/LuchadorNamac.java
+++ GranCombat/src/main/java/GranCombat/modelo/LuchadorNamac.java
@@ -1,5 +1,17 @@
 package GranCombat.modelo;
 
-public class LuchadorNamac {
+public class LuchadorNamac extends Luchador{
 
+	public LuchadorNamac(String nombre, int forz, int cons, int mid, int des, int per) {
+		super(nombre, forz, cons, mid, des, per);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public int puntsResistencia(int constitucio ,int mida){
+		return puntsResistenciaNamac(constitucio, mida, super.per);
+	}
+	
+	public int puntsResistenciaNamac(int constitucio ,int mida, int personalitat){
+		return constitucio+mida+personalitat;
+	}
 }
