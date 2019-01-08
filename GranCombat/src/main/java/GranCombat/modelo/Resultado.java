0a4db67976d9f2a197dfeadfdec03c3b6ae4f9d1
DESKTOP-C491Q1L\LoWs-top <sergio.borrell.flores@gmail.com>
2019-01-08 21:19:26 +0100

    Resultado actualizado con las funciones Ataque y Esquiva

--- GranCombat/src/main/java/GranCombat/modelo/Resultado.java
+++ GranCombat/src/main/java/GranCombat/modelo/Resultado.java
@@ -3,7 +3,7 @@ package GranCombat.modelo;
 public class Resultado {
 	
 	
-	static boolean exitoAtacar (int Probabilidad) {
+	static boolean exitoAccion (int Probabilidad) {
 		int numero;
 		numero = (int) (Math.random() * 100) + 1;
 		if (numero<=Probabilidad) {
@@ -15,8 +15,53 @@ public class Resultado {
 	}
 	
 	
-	//public int combat() {
-		
-	//}
+	public int ataque(int atacar, int danyo) {
+		if (exitoAccion(atacar)){
+			return danyo;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public int esquiva(int esquivaDefensor, int vidaDefensor, int danyoAtacante) {
+		if (exitoAccion(esquivaDefensor)){
+			return vidaDefensor;
+		}
+		else if (vidaDefensor <= danyoAtacante) {
+			return 0;
+		}
+		else {
+			return (vidaDefensor - danyoAtacante);
+		}
+	}
+	
+	/*member function atacar return int is
+			aleatorio int;
+			begin
+				aleatorio := dbms_random.value(1,100);
+				if aleatorio <= SELF.probabilitatatacar() then
+					return SELF.puntsdany();
+				else
+					return 0;
+				end if;
+			end;
+		member function esquivar(vida int,dany int) return int is
+			aleatorio int;
+			resta int;
+			begin
+				aleatorio := dbms_random.value(1,100);
+				if aleatorio <= SELF.probabilitatesquivar() then
+					return vida;
+				else
+					if vida-dany <= 0 then
+						return 0;
+					else
+						resta := vida-dany;
+						return resta;
+					end if;
+				end if;
+			end;
+		end;*/
 
 }
